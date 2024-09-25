ac4e8c06768b8add847de52b59ae9d073f22527e
Arslan <jacklol5760@gmail.com>
2024-09-25 21:52:07 +0500

    Preparation

--- src/components/order/PrintButton.jsx
+++ src/components/order/PrintButton.jsx
@@ -10,7 +10,7 @@ const PrintButton = () => {
     return (
         <button
             onClick={handlePrint}
-            className="bg-green-500 text-white px-4 py-2 rounded-lg"
+            className="bg-green-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-green-600 transition duration-300"
         >
             Печать
         </button>
